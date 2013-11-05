b5e4ac6029119ad24d6f9c5318f8b9f16dadd5c2
Phil Worthington <pworthington27@gmail.com>
2013-11-04 23:33:33 -0800

    Update lib/rulers.rb

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,9 +4,20 @@ require "rulers/routing"
 module Rulers
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
+      end
+      if env['PATH_INFO'] == '/'
+        return [300, {"Location" => '/home/index'}, []]
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(act)
+      begin
+        text = controller.send(act)
+      rescue Exception => e
+        text = e
+      end
       [200, {'Content-Type' => 'text/html'},
         [text]]
     end
