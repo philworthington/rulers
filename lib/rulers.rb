705946a9af7de776aeac41089d635f9a53681109
Phil Worthington <pworthington27@gmail.com>
2013-11-04 22:58:05 -0800

    Build controllers

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,9 +4,21 @@ require "rulers/array"
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers!"]]
+        [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
