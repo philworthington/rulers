f739ff7a15abd6d5121c29c48375d8896f1f819f
Phil Worthington <pworthington27@gmail.com>
2013-11-05 22:51:58 -0800

    Update controllers, views, tests

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,5 +1,8 @@
 require "rulers/version"
 require "rulers/routing"
+require "rulers/util"
+require "rulers/dependencies"
+require "rulers/controller"
 
 module Rulers
   class Application
@@ -20,16 +23,7 @@ module Rulers
       end
       [200, {'Content-Type' => 'text/html'},
         [text]]
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
+      end
     end
   end
 end
